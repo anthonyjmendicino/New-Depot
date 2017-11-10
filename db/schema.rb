beba5942b187d33b0522e0cf6f9bb61d2eb412f3
Anthony Mendicino <anthonyjmendicino@gmail.com>
2017-11-10 09:16:22 +0000

    First commit

--- db/schema.rb
+++ db/schema.rb
@@ -60,11 +60,4 @@ ActiveRecord::Schema.define(version: 20171107175035) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
